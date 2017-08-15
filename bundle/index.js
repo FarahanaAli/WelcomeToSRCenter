f4be83a074aefb669f177b9a55eb167f2cdea3b3
System <none>
2017-08-15 16:52:27 +0000

    Checkpoint

--- bundle/index.js
+++ bundle/index.js
@@ -1,21 +1,27 @@
-const Marionette = require('backbone.marionette')
+const Marionette = require('backbone.marionette');
+const appTpl = require('./app.hbs');
+
+console.log(appTpl);
 
 const RootView = Marionette.View.extend({
+  template: appTpl,
+  
   onShow() {
-    this.$el.
+    this.$el.innerHTML = 'yo';
   }
 })
 
 const App = Marionette.Application.extend({
   region: '#root',
+  
   onStart() {
     this.showView(new RootView())
   }
 })
 
-const app = new Marionette.Application();
+const app = new App();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
   
+  app.start();
 })
