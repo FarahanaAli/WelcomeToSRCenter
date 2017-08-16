883d3dab464e756804392875b70a38a90c0b3579
System <none>
2017-08-16 14:10:27 +0000

    Checkpoint

--- data/urls.js
+++ data/urls.js
@@ -1,30 +1,18 @@
 module.exports = {
-  videoUrls: [
+  videoEmbeds: [
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/TxXKQXdEej8" frameborder="0" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/MjqdNAFUECg" frameborder="0" allowfullscreen></iframe>',
+  ],<iframe width="560" height="315" src="https://www.youtube.com/embed/kRy5kRB93zA" frameborder="0" allowfullscreen></iframe>
+  imageUrls: [
+    'http://imgur.com/a/QYssX',
+    'http://imgur.com/a/9B0d0',
+    'https://goo.gl/images/bnLIuU',
+    'http://imgur.com/a/6ilLu',
+    'http://imgur.com/a/ZxSxg',
+    'http://imgur.com/a/MpL9Z',
+    'http://imgur.com/a/N9S2c',
+    'http://imgur.com/a/MpL9Z',
+    'http://imgur.com/a/RDaVN',
+    ''
   ],
-  imageUrls: [http://imgur.com/a/QYssX
-    
-  ],
-  imageUrls: [http://imgur.com/a/9B0d0
-  
-  ],
-  imageUrls: [https://goo.gl/images/bnLIuU
-  
-  ],
-  imageUrls: [http://imgur.com/a/6ilLu
-              
-  ],
-  imageUrls: [http://imgur.com/a/ZxSxg
-             
-  ],
-  imageUrls: [http://imgur.com/a/MpL9Z
-             
-  ],
-  imageUrls: [http://imgur.com/a/N9S2c
-             
-  ],
-  imageUrls: [http://imgur.com/a/MpL9Z
-             
-  ],
-  imageUR
 };
